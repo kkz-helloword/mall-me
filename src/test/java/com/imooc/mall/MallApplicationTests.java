c62b31782249bf3d0684705e1c11726259c43867
zhangkai <260141532@qq.com>
2021-11-12 16:23:36 +0800

    hello git6

--- src/test/java/com/imooc/mall/MallApplicationTests.java
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -14,6 +14,7 @@ public class MallApplicationTests {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git6");
     }
 
 }
