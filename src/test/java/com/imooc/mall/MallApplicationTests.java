d120bd03f1ac7a972762c0c985fc17883830e7a1
zhangkai <260141532@qq.com>
2021-11-12 15:36:18 +0800

    hello git

---
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -0,0 +1,18 @@
+package com.imooc.mall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)//让测试在Spring容器环境下执行。如测试类中无此注解，将导致service,dao等自动注入失败。
+@SpringBootTest//测试类需要，空指针也可运行,运行tomcat
+public class MallApplicationTests {
+
+    @Test
+    public void load(){
+        System.out.println("hello git");
+    }
+
+}
+
